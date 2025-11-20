7159817d3869bb479b2c96c1e8f98a202aacdbef
unknown <nhesenov029@gmail.com>
2025-11-21 01:41:23 +0400

    C:/Program Files/Git/

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({ message: "User REST API işləyir!", task: "Task 4" });
+  res.json({ message: "User REST API is working!", task: "Task 4" });
 });
 
 app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
-  console.log(`API işləyir → http://localhost:${PORT}`);
+  console.log(`API is running → http://localhost:${PORT}`);
   console.log(`Userlər → http://localhost:${PORT}/users`);
 });
